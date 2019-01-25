00e18492182684363a6d8d7b7ae553c95ae12a73
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-01-26 01:11:41 +0800

    Create the Header component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header.vue'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Header
+  }
 }
 </script>
 
@@ -16,6 +22,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: $textColorPrimary;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+<header class="header">
+  <div class="container">
+    <img src="@/assets/logo.png" class="logo">
+    <nav id="desktop-menu">
+      <a
+        href="#"
+        v-for="(menu, index) in menuList"
+        :key="index"
+        :class="{ active: menu === activeMenu }"
+      >
+        {{menu}}
+      </a>
+    </nav>
+  </div>
+</header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      menuList: ['Home', 'Model', 'Casting', 'Rankings', 'Industry Members'],
+      activeMenu: 'Home'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  background: #ffffff;
+  height: 70px;
+}
+
+.container {
+  align-items: center;
+  display: flex;
+  height: 100%;
+  margin: auto;
+  max-width: 1280px;
+}
+
+.logo {
+  display: inline-block;
+  height: 100%;
+}
+
+#desktop-menu {
+  flex-grow: 1;
+  text-align: right;
+
+  a {
+    color: $textColorPrimary;
+    font-size: 18px;
+    margin-right: 32px;
+    text-decoration: none;
+    transition: all .3s ease-in-out;
+
+    &:hover, &.active {
+      color: $colorPrimary;
+    }
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+}
+</style>
