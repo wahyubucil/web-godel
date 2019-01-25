4a2215bbb9d83f79a5cddff0a851ae699129f981
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-01-26 02:33:34 +0800

    Complete Banner component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <Header />
-    <div id="main"></div>
+    <div id="main">
+      <Banner />
+    </div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import Banner from '@/components/Banner.vue'
 
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    Banner
   }
 }
 </script>
---
+++ src/components/Banner.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="banner">
+    <parallax :speed-factor="0.3">
+      <img src="@/assets/banner-photo.jpg">
+    </parallax>
+    <div class="banner-content">
+      <h1>Famous Is Easy!</h1>
+      <h2>Cara Mudah <i>Menjadi</i> dan <i>Mencari</i> Model</h2>
+      <div class="button-content">
+        <a href="#" v-for="(text, index) in buttonText" :key="index">{{text}}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Parallax from 'vue-parallaxy'
+
+export default {
+  name: 'Banner',
+  data () {
+    return {
+      buttonText: ['Saya Seorang Model', 'Saya Mencari Model']
+    }
+  },
+  components: {
+    Parallax
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#banner {
+  height: calc(100vh - 70px);
+  overflow: hidden;
+  position: relative;
+}
+
+.banner-content {
+  align-items: center;
+  background: rgba($textColorPrimary, .7);
+  color: #ffffff;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+
+  h1 {
+    font-size: 58px;
+    margin: 0;
+    text-transform: uppercase;
+  }
+
+  h2 {
+    font-size: 24px;
+    margin-top: 10px;
+  }
+}
+
+.button-content {
+  a {
+    background: #af0404;
+    color: #ffffff;
+    display: inline-block;
+    font-size: 18px;
+    margin-right: 16px;
+    padding: 15px 30px;
+    text-decoration: none;
+    text-transform: uppercase;
+    transition: all .3s ease-in-out;
+
+    &:last-child {
+      margin-right: 0;
+    }
+
+    &:hover {
+      background: $colorPrimary;
+    }
+  }
+}
+</style>
